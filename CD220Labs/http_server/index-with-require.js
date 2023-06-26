05dc83ce1e6c31482db6145a742bc79c54622da6
Mouctar Bah <mouctar.mbah995@gmail.com>
2023-06-26 07:24:10 -0400

    initial commit

--- CD220Labs/http_server/index-with-require.js
+++ CD220Labs/http_server/index-with-require.js
@@ -1,12 +1,11 @@
-const http = require('http');
-const today = require('./today');
+const http = require("http");
+const today = require("./today");
 
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end(`Hello, World! The date today is ${today.getDate()}`);
-}
+};
 
 const port = 8080;
 const server = http.createServer(requestListener);
-console.log('server listening on port: ' + port);
-server.listen(port);
+console.log("server listening on port: " + port);
